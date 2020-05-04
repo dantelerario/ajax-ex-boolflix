c3ee601593db9220b91c1d81c8625b3f4a5ac162
Dante L'Erario <dantelerario@gmail.com>
2020-05-04 14:59:39 +0200

    log OK

--- js/main.js
+++ js/main.js
@@ -5,16 +5,22 @@ $(document).ready( function() {
   var btnSearch = $('.btnSearch');
   // loading API and search
   btnSearch.on('click', function() {
+    var searchInput = boxSearch.val().trim().toLowerCase();
     $.ajax({
-      url: 'https://api.themoviedb.org/3/search/movie?api_key=80fa425ed2ac212a9eb2eafb6396d967',
+      url: 'https://api.themoviedb.org/3/search/movie',
       method: 'GET',
+      data: {
+        api_key: '80fa425ed2ac212a9eb2eafb6396d967',
+        language: 'it-IT',
+        query: searchInput,
+      },
       success: function(data) {
         // HANDLEBARS
         var source = $('#template').html();
         var template = Handlebars.compile(source);
-        var htmlMoviesList = template(data.response);
-        cdList.append(htmlMoviesList);
-
+        var htmlMoviesList = template(data.results);
+        moviesList.append(htmlMoviesList);
+        console.log(data.results);
       },
       error: function() {
         console.log('ERROR');
