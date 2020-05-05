deb28c3281e73657cf3e1f54f72378633fb19fe4
Dante L'Erario <dantelerario@gmail.com>
2020-05-05 12:10:08 +0200

    JS: miss the Stars and the Flags

--- js/main.js
+++ js/main.js
@@ -24,11 +24,20 @@ $(document).ready( function() {
         console.log(data.results); //debug
         for (i = 0; i < moviesSuccessList.length; i++) {
           var movies = moviesSuccessList[i];
+          var starVote = (movies.vote_average / 2);
+          var roundStarVote = Math.round(starVote);
+          if (movies.original_language === 'en') {
+            var flagLanguage = "";
+          } else if (movies.original_language === 'it') {
+            var flagLanguage = "";
+          } else {
+            var flagLanguage = tv.original_language;
+          }
           var results = {
             title: movies.title,
             original_title: movies.original_title,
-            original_language: movies.original_language,
-            vote_average: movies.vote_average,
+            original_language: flagLanguage,
+            vote_average: roundStarVote,
             type: 'MOVIE',
           }
           var htmlMoviesList = template(results);
@@ -57,11 +66,20 @@ $(document).ready( function() {
         console.log(data.results); //debug
         for (i = 0; i < tvSuccessList.length; i++) {
           var tv = tvSuccessList[i];
+          var starVote = (tv.vote_average / 2);
+          var roundStarVote = Math.round(starVote);
+            if (tv.original_language === 'en') {
+              var flagLanguage = "";
+            } else if (tv.original_language === 'it') {
+              var flagLanguage = "";
+            } else {
+              var flagLanguage = tv.original_language;
+            }
           var results = {
             title: tv.name,
             original_title: tv.original_name,
-            original_language: tv.original_language,
-            vote_average: tv.vote_average,
+            original_language: flagLanguage,
+            vote_average: roundStarVote,
             type: 'TV',
           }
           var htmlTvList = template(results);
